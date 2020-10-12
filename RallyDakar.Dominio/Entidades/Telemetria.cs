ed76ff158a4b6bc0aaee22cb0b5beb4ee03b858a
Junior Chicocki <juniorchicocki@gmail.com>
2020-10-12 15:28:23 -0300

    Add project files.

---
+++ RallyDakar.Dominio/Entidades/Telemetria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RallyDakar.Dominio.Entidades
+{
+    public class Telemetria
+    {
+        public int Id { get; set; }
+        public int TemporadaId { get; set; }
+        public int PilotoId { get; set; }
+        public DateTime Data { get; set; }
+        public TimeSpan Hora { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal PercentualCombustivel { get; set; }
+        public decimal Velicidade { get; set; }
+        public decimal Rpm { get; set; }
+        public int TemperatudaMotor { get; set; }
+        public int TemperaturaExterna { get; set; }
+        public Decimal AltidudeNivelMar { get; set; }
+        public bool PedalAcelerador { get; set; }
+        public bool PedalFreio { get; set; }
+    }
+}
